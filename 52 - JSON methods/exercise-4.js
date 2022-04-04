2f5a009951602e9ec89209d4a615e676253f90a0
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-04-04 17:15:37 +0200

    Update exercise-4.js

--- 52 - JSON methods/exercise-4.js
+++ 52 - JSON methods/exercise-4.js
@@ -2,15 +2,14 @@ const person = {
   id: 1,
   firstName: 'Mario',
   lastName: 'Rossi',
-  age: 25,
-  replacer(key, value) {
-    if (typeof value === "string") {
-      return undefined;
-    }
-    return value;
-  }
+  age: 25
 };
 
-const json = JSON.stringify(person, ["id", "age"]);
+const json = JSON.stringify(person, (key, value) => {
+  if (typeof value === "string") {
+    return undefined;
+  }
+  return value;
+});
 
 console.log(json); // Should return: { id: 1, age: 25 }
